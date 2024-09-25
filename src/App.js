fc1f6f8c5e6321a7895045688e07875b390f47db
Pavun786 <pulsarpavuns786@gmail.com>
2024-09-25 07:48:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import GetAllProducts from './productComponent/getAllProducts';
+import { Routes,Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import GetCartItems from './productComponent/getCartItems';
+import Register from './Auth/Register';
+import Login from './Auth/Login';
+import SearchData from './SearchData';
+import GetSingleProduct from './productComponent/getSingleProduct';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+     <Routes>
+      <Route path="/getAll" element={<GetAllProducts/>}/>
+      <Route path="/cart" element={<GetCartItems/>}/>
+      <Route path="/search" element={<SearchData/>}/>
+      <Route path='/single/:id' element={<GetSingleProduct/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/' element={<Login/>}/>
+     </Routes>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom"
+import { SearchProvider } from './Context/Search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <SearchProvider>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    </SearchProvider>
   </React.StrictMode>
 );
 
